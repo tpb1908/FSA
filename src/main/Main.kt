b13a39615a9843710cf6e020c05cbac686b6e4f3
Theo Pearson-Bray <tpb1908dev@gmail.com>
2018-05-15 19:44:08 +0100

    Add twenty five pence machine example.

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -6,11 +6,8 @@ import java.io.File
 
 class Main {
 
-    /*
-    TODO
-    - Add tests
 
-     */
+     // TODO Mealy machine
 
     companion object {
 
@@ -19,20 +16,89 @@ class Main {
             if (args.isNotEmpty()) {
                 val fsaString = File(args.first()).readText()
                 parser.parse(fsaString)
-            } else {
-
             }
 
-            val simple = "{state_1}{s} [{a}{state_2} {b}{state_3}], {state_2}{e} [], {state_3}{e} [{a}{state_1}]"
+            val twentyfivepencemachine = """{
+                "states": [
+                    {
+                        "name": "start",
+                        "start": true,
+                        "transitions": [
+                            {
+                                "key": "5p",
+                                "state": "5"
+                            },
+                            {
+                                "key": "10p",
+                                "state": "10"
+                            },
+                            {
+                                "key": "20p",
+                                "state": "20"
 
-            //This format is horrible. Will fix it eventually
+                            }
+                        ]
+                    },
+                    {
+                        "name": "5",
+                        "transitions": [
+                            {
+                                "key": "5p",
+                                "state": "10"
+                            },
+                            {
+                                "key": "10p",
+                                "state": "15"
+                            },
+                            {
+                                "key": "20p",
+                                "state": "25"
 
-            val fifthchar = "{state_0}{s} [{0}{state_0}{1}{state_0}{1}{state_1}], " +
-                    "{state_1} [{0}{state_2}{1}{state_2}], " +
-                    "{state_2} [{0}{state_3}{1}{state_3}]," +
-                    "{state_3} [{0}{state_4}{1}{state_4}]," +
-                    "{state_4} [{0}{state_5}{1}{state_5}], " +
-                    "{state_5}{e} []"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "10",
+                        "transitions": [
+                            {
+                                "key": "5p",
+                                "state": "15"
+                            },
+                            {
+                                "key": "10p",
+                                "state": "20"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "15",
+                        "transitions": [
+                            {
+                                "key": "5p",
+                                "state": "20"
+                            },
+                            {
+                                "key": "10p",
+                                "state": "25"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "20",
+                        "transitions": [
+                            {
+                                "key": "5p",
+                                "state": "25"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "25",
+                        "end": true,
+                        "transitions": []
+                    }
+                ]
+                }"""
 
             val fifthcharJson =  """{
                     "states": [
@@ -114,14 +180,15 @@ class Main {
                     ]
                 }"""
 
-            //parser.debug = false
-            val fsa = parser.parse(fifthcharJson)
-            println("FSA is $fsa")
+
+
+            val fsa = parser.parse(twentyfivepencemachine)
+            println("FSA $fsa")
             println("\n\n\n")
             val runner  = Runner(fsa)
             runner.debug = true
             runner.saveStateHistory = false
-            runner.run(arrayOf("0", "1", "1", "1", "1", "1"))
+            runner.run(arrayOf("5p", "5p", "5p", "10p"))
             runner.states.forEach {
                 if (it is Runner.RunningState.HistoryRunningState) {
                     println("History for state ${it.state} is ${it.previous}")
